ef7f362b2d270f8bda06cef53c63a09b3739ac45
Mark <mindmark@gmail.com>
2013-08-13 16:08:35 -0700

    cleaned up encoding example

--- examples/encode.py
+++ examples/encode.py
@@ -1,97 +1,114 @@
 import sys
 import av
 
+# open input file
+input_file_path = sys.argv[1]
+input_file = av.open(input_file_path)
 
-source = sys.argv[1]
-source_video = av.open(source)
+input_video_stream = None
+input_audio_stream = None
 
+input_streams = []
 
-streams = [s for s in source_video.streams if s.type == b'video']
-audio_streams = [s for s in source_video.streams if s.type == b'audio']
-streams = [streams[0]]
+# find first video stream
+for stream in input_file.streams:
+    if stream.type == b'video':
+        input_video_stream = stream
+        input_streams.append(input_video_stream)
+        break
 
-if audio_streams:
-    streams.append(audio_streams[0])
+# find first audio stream
+for stream in input_file.streams:
+    if stream.type == b'audio':
+        input_audio_stream = stream
+        input_streams.append(input_audio_stream)
+        break
+
+# open output file
+output_file_path = "./sandbox/encode_example.mp4"
+output_file = av.open(output_file_path, 'w')
+
+output_video_stream = None
+output_audio_stream = None
+
+if input_video_stream:
+    
+    # setup video output stream
+    output_video_stream = output_file.add_stream("h264",input_video_stream.base_frame_rate)
+    
+    codec = output_video_stream.codec
+    
+    # set output size
+    codec.width = 1280
+    codec.height = 720
+    
+    # set bit rate and pix_fmt
+    codec.bit_rate = 256000
+    codec.pix_fmt = "yuv420p"
+    
     
 
-encode_video = av.open("./sandbox/encode_example.mp4", 'w')
+if input_audio_stream:
+    
+    # setup audio output stream
+    output_audio_stream = output_file.add_stream("mp3")
+    
+    codec = output_audio_stream.codec
+    
+    codec.bit_rate = 128000
+    codec.sample_rate = 44100
+    codec.channel_layout = "stereo"
+    
+    codec.sample_fmt = "s16p"
+    
 
-
-video_stream = encode_video.add_stream("h264",streams[0].base_frame_rate)
-audio_stream = encode_video.add_stream("mp3")
-
-codec = video_stream.codec
-print "name", codec.name
-print "bit_rate", codec.bit_rate
-print "time_base", codec.time_base
-print "pix_fmt", codec.pix_fmt
-print  "size %ix%i" % (codec.width, codec.height)
-print "gop_size", codec.gop_size
-
-print "Audio"
-print "sample format", audio_stream.codec.sample_fmt
-print "channel layout", audio_stream.codec.channel_layout
-print "channels", audio_stream.codec.channels
-#audio_stream.codec.channel_layout = "mono"
-
-print "channel layout",  audio_stream.codec.channel_layout 
-print "channels", audio_stream.codec.channels
-
-#audio_stream.codec.sample_fmt = "s16p"
-#audio_stream.codec.channel_layout  = "mono"
-#audio_stream.codec.channels = 2
-#audio_stream.codec.sample_fmt = 's16p'
-print "channel layout", audio_stream.codec.channel_layout 
-
-#raise Exception('stop')
-encode_video.dump()
-
-#raise Exception()
-
-
+output_file.dump()
 
 frame_count = 0
 
-for packet in source_video.demux(streams):
+# demux the input file
+for packet in input_file.demux(input_streams):
+    
+    # decode the frames in the packet
     for frame in packet.decode():
         
+        encoded_packet = None
+        
         if packet.stream.type == b'audio':
-            #print frame
-            #print frame.samples, frame.sample_fmt, frame.channels, frame.channel_layout
-            encoded_packet = audio_stream.encode(frame)
-            if encoded_packet:
-                encode_video.mux(encoded_packet)
+            # encode audio frame
+            encoded_packet = output_audio_stream.encode(frame)
         else:
-            #print frame.pix_fmt
-            frame_count += 1
-            #print frame_count
-            encoded_packet = video_stream.encode(frame)
-            if encoded_packet:
-                pass
-                encode_video.mux(encoded_packet)
+            # encode video frame
+            encoded_packet = output_video_stream.encode(frame)
             print frame_count
+            frame_count += 1
+            
+        if encoded_packet:
+                # Add encoded packet to output file
+                output_file.mux(encoded_packet)
         
     if frame_count > 1000:
         break
 
-
+# Finally we need to flush out the frames that are buffered in the encoder.
+# To do that we simply call encode with no args until we get a None returned
 while True:
-    packet =  audio_stream.encode()
-    print "flushed out audio packet", packet
+    packet =  output_audio_stream.encode()
     if packet:
-        encode_video.mux(packet)
-    else:
-
-        break
-
-while True:
-    packet =  video_stream.encode()
-    print "flushed out video packet", packet
-    if packet:
-        encode_video.mux(packet)
+        print "flushed out audio packet", packet
+        output_file.mux(packet)
     else:
         break
 
-  
-         
-encode_video.close()
+while True:
+    packet =  output_video_stream.encode()
+    if packet:
+        print "flushed out video packet", packet
+        output_file.mux(packet)
+    else:
+        break
+
+# now close the files
+# if you don't close the output file the trailer will not be written
+input_file.close()
+output_file.close()
