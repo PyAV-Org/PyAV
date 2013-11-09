74688aa2cc534f013b957885d9f7cf423cbfc2cd
Mike Boers <github@mikeboers.com>
2013-11-08 20:50:21 -0800

    Import many of the things into `av`

--- av/__init__.py
+++ av/__init__.py
@@ -3,9 +3,19 @@
 # library that is being wrapped.
 from ._core import time_base
 
-# Capture logging.
+# Capture logging (by importing it).
 from . import logging
 
-# For convenience.
-from .context import Context as open
+# For convenience, IMPORT ALL OF THE THINGS (that are constructable by the user).
+from .audio.fifo import AudioFifo
+from .audio.format import AudioFormat
+from .audio.frame import AudioFrame
+from .audio.layout import AudioLayout
+from .context import Context
 from .utils import AVError
+from .video.format import VideoFormat
+from .video.frame import VideoFrame
+
+
+def open(*args, **kwargs):
+    return Context(*args, **kwargs)
