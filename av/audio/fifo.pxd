a347095a3b0a5509a8e7a53da3ec80106baa3eab
Mike Boers <github@mikeboers.com>
2013-11-10 11:33:48 -0800

    The FIFO example works and plays

--- av/audio/fifo.pxd
+++ av/audio/fifo.pxd
@@ -2,6 +2,8 @@ from libc.stdint cimport int64_t, uint64_t
 
 cimport libav as lib
 
+from av.audio.layout cimport AudioLayout
+from av.audio.format cimport AudioFormat
 from av.audio.frame cimport AudioFrame
 
 
@@ -9,17 +11,13 @@ cdef class AudioFifo:
 
     cdef lib.AVAudioFifo *ptr
     
-    cdef uint64_t channel_layout_
-    cdef int channels_
-    cdef bint add_silence
-    
-    cdef lib.AVSampleFormat sample_fmt_
-    cdef int sample_rate_
-    
+    cdef readonly AudioFormat format
+    cdef readonly AudioLayout layout
+        
     cdef int64_t last_pts
     cdef int64_t pts_offset
-    cdef lib.AVRational time_base_
+    cdef lib.AVRational time_base
     
     cpdef write(self, AudioFrame frame)
-    cpdef read(self, int nb_samples=*)
+    cpdef read(self, unsigned int nb_samples=*, bint partial=*)
     
--- examples/fifo.py
+++ examples/fifo.py
@@ -8,6 +8,15 @@ import Image
 import av
 
 
+def print_data(frame):
+    for i, plane in enumerate(frame.planes or ()):
+        data = plane.to_bytes()
+        print '\tPLANE %d, %d bytes' % (i, len(data))
+        data = data.encode('hex')
+        for i in xrange(0, len(data), 128):
+            print '\t\t\t%s' % data[i:i + 128]
+
+
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('path')
 arg_parser.add_argument('-p', '--play', action='store_true')
@@ -21,37 +30,45 @@ ffplay = None
 container = av.open(args.path)
 stream = next(s for s in container.streams if s.type == 'audio')
 
-frame_count = 0
+fifo = av.AudioFifo()
+
+input_count = 0
+output_count = 0
+
 for i, packet in enumerate(container.demux(stream)):
     for frame in packet.decode():
-        frame_count += 1
+        input_count += 1
         
-        if args.play:
-            if not ffplay:
-                cmd = ['ffplay',
-                    '-f', 's16le',
-                    '-ar', str(stream.rate),
-                    '-vn','-',
-                ]
-                print '*** ****', ' '.join(cmd)
-                ffplay = subprocess.Popen(cmd, stdin=subprocess.PIPE)
-            try:
-                ffplay.stdin.write(frame.planes[0].to_bytes())
-            except IOError as e:
-                print e
-                exit()
-
-        print '<<< %04d' % i, frame
+        print '<<< %04d     ' % i, frame
 
         if args.data:
-            print '\t\tdata'
-            for i, plane in enumerate(frame.planes or ()):
-                data = plane.to_bytes()
-                print '\t\t\tPLANE %d, %d bytes' % (i, len(data))
-                data = data.encode('hex')
-                for i in xrange(0, len(data), 128):
-                    print '\t\t\t%s' % data[i:i + 128]
+            print_data(frame)
 
-        if args.count and frame_count >= args.count:
+        fifo.write(frame)
+        while frame:
+            frame = fifo.read(args.size)
+            if frame:
+                output_count += 1
+                print '>>>      %04d' % output_count, frame
+                if args.data:
+                    print_data(frame)
+
+                if args.play:
+                    if not ffplay:
+                        cmd = ['ffplay',
+                            '-f', 's16le',
+                            '-ar', str(stream.rate),
+                            '-vn','-',
+                        ]
+                        print '*** ****', ' '.join(cmd)
+                        ffplay = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+                    try:
+                        ffplay.stdin.write(frame.planes[0].to_bytes())
+                    except IOError as e:
+                        print e
+                        exit()
+
+
+        if args.count and input_count >= args.count:
             exit()
 
