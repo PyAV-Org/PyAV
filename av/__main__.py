40b3cb85f1d1b1823fdb24dd0aaa01c14d005d3c
Mike Boers <github@mikeboers.com>
2015-02-01 09:16:06 -0800

    Python 3 strikes again!

--- av/__main__.py
+++ av/__main__.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import argparse
 
 
@@ -14,9 +16,9 @@ def main():
 
         import av._core
 
-        print 'PyAV v' + av._core.pyav_version
-        print 'git origin: git@github.com:mikeboers/PyAV'
-        print 'git commit:', av._core.pyav_commit
+        print('PyAV v' + av._core.pyav_version)
+        print('git origin: git@github.com:mikeboers/PyAV')
+        print('git commit:', av._core.pyav_commit)
 
         by_config = {}
         for libname, config in sorted(av._core.versions.iteritems()):
@@ -27,11 +29,11 @@ def main():
                     []
                 ).append((libname, config))
         for (config, license), libs in sorted(by_config.iteritems()):
-            print 'library configuration:', config
-            print 'library license:', license
+            print('library configuration:', config)
+            print('library license:', license)
             for libname, config in libs:
                 version = config['version']
-                print '%-13s %3d.%3d.%3d' % (libname, version[0], version[1], version[2])
+                print('%-13s %3d.%3d.%3d' % (libname, version[0], version[1], version[2]))
 
     if args.codecs:
         from av.codec import dump_codecs
