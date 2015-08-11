c6162bda9ec1f4ccdc813aabf2208bfc0c835f08
Mike Boers <westernx@mikeboers.com>
2015-08-11 16:05:10 -0700

    Move ByteSource out of utils

--- av/utils.pyx
+++ av/utils.pyx
@@ -1,7 +1,5 @@
 from libc.stdint cimport int64_t, uint8_t, uint64_t
 
-from cpython.buffer cimport PyObject_CheckBuffer, PyObject_GetBuffer, PyBUF_SIMPLE, PyBuffer_Release
-
 from fractions import Fraction
 from threading import local
 import sys
@@ -128,47 +126,6 @@ cdef object to_avrational(object value, lib.AVRational *input):
 
 cdef object av_frac_to_fraction(lib.AVFrac *input):
     return Fraction(input.val * input.num, input.den)
-
-
-
-# === BYTE SOURCES ===
-# ====================
-
-cdef class ByteSource(object):
-
-    def __cinit__(self, owner):
-        self.owner = owner
-
-        try:
-            self.ptr = owner
-        except TypeError:
-            pass
-        else:
-            self.length = len(owner)
-            return
-
-        if PyObject_CheckBuffer(owner):
-            res = PyObject_GetBuffer(owner, &self.view, PyBUF_SIMPLE)
-            if not res:
-                self.has_view = True
-                self.ptr = <unsigned char *>self.view.buf
-                self.length = self.view.len
-                return
-        
-        raise TypeError('expected bytes, bytearray or memoryview')
-
-    def __dealloc__(self):
-        if self.has_view:
-            PyBuffer_Release(&self.view)
-
-
-cdef ByteSource bytesource(obj, bint allow_none=False):
-    if allow_none and obj is None:
-        return
-    elif isinstance(obj, ByteSource):
-        return obj
-    else:
-        return ByteSource(obj)
 
 
 
