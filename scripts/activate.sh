15fe359e9e68c72386f500450aef453ca088e747
Mike Boers <westernx@mikeboers.com>
2015-08-18 12:31:50 -0700

    Lets try OS X on Travis

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,29 +1,56 @@
 #!/bin/bash
 
+# Make sure this is sourced.
+if [[ "$0" == "${BASH_SOURCE[0]}" ]]; then
+    echo This must be sourced.
+    exit 1
+fi
+
 export PYAV_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.."; pwd)"
 
 
-PYAV_LIBRARY_NAME="${PYAV_LIBRARY_NAME-$LIBRARY}"
-PYAV_LIBRARY_VERSION="${PYAV_LIBRARY_VERSION-$LIBRARY_VERSION}"
+PYAV_LIBRARY_NAME="${PYAV_LIBRARY_NAME}"
+PYAV_LIBRARY_VERSION="${PYAV_LIBRARY_VERSION}"
 
 if [[ ! "$PYAV_LIBRARY_NAME" ]]; then
-    PYAV_LIBRARY_NAME=ffmpeg
-    echo "No \$LIBRARY set; defaulting to $PYAV_LIBRARY_NAME"
+    # We allow $FFMPEG and $LIBAV on Travis to make the build matrix pretty.
+    if [[ "$TRAVIS" && "$FFMPEG" ]]; then
+        PYAV_LIBRARY_NAME=ffmpeg
+        PYAV_LIBRARY_VERSION="$FFMPEG"
+    elif [[ "$TRAVIS" && "$LIBAV" ]]; then
+        PYAV_LIBRARY_NAME=libav
+        PYAV_LIBRARY_VERSION="$LIBAV"
+
+    # Pull from command line argument.
+    elif [[ "$1" ]]; then
+        PYAV_LIBRARY_VERSION="$1"
+    else
+        PYAV_LIBRARY_VERSION=ffmpeg
+        echo "No \$PYAV_LIBRARY_NAME set; defaulting to $PYAV_LIBRARY_NAME"
+    fi
 fi
 
 if [[ ! "$PYAV_LIBRARY_VERSION" ]]; then
-    case "$PYAV_LIBRARY_NAME" in
-        ffmpeg)
-            PYAV_LIBRARY_VERSION=2.7
-            ;;
-        libav)
-            PYAV_LIBRARY_VERSION=11.4
-            ;;
-        *)
-            echo Unknown \$LIBRARY \"$LIBRARY\"; exiting
-            exit 1
-    esac
-    echo "No \$LIBRARY_VERSION set; defaulting to $PYAV_LIBRARY_VERSION"
+
+    # Pull from command line argument.
+    if [[ "$2" ]]; then
+        PYAV_LIBRARY_VERSION="$2"
+
+    # Defaults.
+    else
+        case "$PYAV_LIBRARY_NAME" in
+            ffmpeg)
+                PYAV_LIBRARY_VERSION=2.7
+                ;;
+            libav)
+                PYAV_LIBRARY_VERSION=11.4
+                ;;
+            *)
+                echo "Unknown \$PYAV_LIBRARY_NAME \"$PYAV_LIBRARY_NAME\"; exiting"
+                exit 1
+        esac
+        echo "No \$PYAV_LIBRARY_VERSION set; defaulting to $PYAV_LIBRARY_VERSION"
+    fi
 fi
 
 export PYAV_LIBRARY_NAME
@@ -47,8 +74,4 @@ source "$PYAV_VENV/bin/activate"
 export _PYAV_ACTIVATED=1
 
 
-# If this is executed, then execute our args.
-if [[ -e "$0" ]]; then
-    exec "$@"
-fi
 
