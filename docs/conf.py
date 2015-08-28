6561bf6bed7db181ca2ca3057cfa5fb2c98d464d
Mike Boers <github@mikeboers.com>
2015-08-28 16:18:42 -0700

    Favicon and logo for docs

--- docs/conf.py
+++ docs/conf.py
@@ -127,12 +127,12 @@ html_theme = 'nature'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'logo-250.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'favicon.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
