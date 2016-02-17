bfd9e2d35743dbb3a59d561cee4cbeb6b0ee325a
Tim Ahpee <timah@blackmagicdesign.com>
2016-02-17 11:49:14 +1100

    Added simplified entries for channel counts greater than 8

--- av/audio/layout.pyx
+++ av/audio/layout.pyx
@@ -15,7 +15,7 @@ cdef AudioLayout get_audio_layout(int channels, uint64_t c_layout):
 
 
 # These are the defaults given by FFmpeg; Libav is different.
-cdef uint64_t default_layouts[9]
+cdef uint64_t default_layouts[17]
 default_layouts[0] = 0
 default_layouts[1] = lib.AV_CH_LAYOUT_MONO
 default_layouts[2] = lib.AV_CH_LAYOUT_STEREO
@@ -25,6 +25,14 @@ default_layouts[5] = lib.AV_CH_LAYOUT_5POINT0_BACK
 default_layouts[6] = lib.AV_CH_LAYOUT_5POINT1_BACK
 default_layouts[7] = lib.AV_CH_LAYOUT_6POINT1
 default_layouts[8] = lib.AV_CH_LAYOUT_7POINT1
+default_layouts[9] = 0x01FF
+default_layouts[10] = 0x03FF
+default_layouts[11] = 0x07FF
+default_layouts[12] = 0x0FFF
+default_layouts[13] = 0x1FFF
+default_layouts[14] = 0x3FFF
+default_layouts[15] = 0x7FFF
+default_layouts[16] = 0xFFFF
 
 
 # These are the descriptions as given by FFmpeg; Libav does not have them.
