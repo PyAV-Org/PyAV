dd10bbb32a56566c195eef5e7bbfe1c91bb02617
Wel C. van der <wel@Physics.LeidenUniv.nl>
2016-09-17 20:48:49 +0200

    FIX VC10 Build (Py33, 34)

--- setup.py
+++ setup.py
@@ -209,6 +209,14 @@ def new_compiler(*args, **kwargs):
     # steps before the process is spawned, we can't monkey-patch msvc compileres.
     if not is_msvc(cc) and kwargs.pop('silent', True):
         cc.spawn = _CCompiler_spawn_silent
+    # If MSVC10, initialize the compiler here and add /MANIFEST to linker flags.
+    # See Python issue 4431 (https://bugs.python.org/issue4431)
+    if is_msvc(cc):
+        from distutils.msvc9compiler import get_build_version
+        if get_build_version() == 10:
+            cc.initialize()
+            for ldflags in [cc.ldflags_shared, cc.ldflags_shared_debug]:
+                unique_extend(ldflags, ['\MANIFEST'])
     return cc
 
 
