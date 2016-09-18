b737c6ceb6750d00f62dfdaa40fee3e757c680a3
Mark Reid <mindmark@gmail.com>
2016-09-17 19:13:59 -0700

    use new encoding and decoding classes

--- av/audio/stream.pyx
+++ av/audio/stream.pyx
@@ -43,25 +43,8 @@ cdef class AudioStream(Stream):
     property channels:
         def __get__(self):
             return self._codec_context.channels
-        
-    cdef _decode_one(self, lib.AVPacket *packet, int *data_consumed):
 
-        if not self.next_frame:
-            self.next_frame = alloc_audio_frame()
-
-        cdef int completed_frame = 0
-        data_consumed[0] = err_check(lib.avcodec_decode_audio4(self._codec_context, self.next_frame.ptr, &completed_frame, packet))
-        if not completed_frame:
-            return
-        
-        cdef AudioFrame frame = self.next_frame
-        self.next_frame = None
-        
-        frame._init_properties()
-        
-        return frame
-    
-    cpdef encode(self, AudioFrame input_frame):
+    def encode(self, AudioFrame input_frame=None):
         """Encodes a frame of audio, returns a packet if one is ready.
         The output packet does not necessarily contain data for the most recent frame, 
         as encoders can delay, split, and combine input frames internally as needed.
@@ -74,83 +57,59 @@ cdef class AudioStream(Stream):
             self.resampler = AudioResampler(
                 self.format,
                 self.layout,
-                self._codec_context.sample_rate
-            )
-        if not self.fifo:
-            self.fifo = AudioFifo()
+                self._codec_context.sample_rate)
 
-        # if input_frame:
-        #     print 'input_frame.ptr.pts', input_frame.ptr.pts
+        cdef AudioFrame resampled_frame
+        if input_frame:
+            resampled_frame = self.resampler.resample(input_frame)
+        else:
+            resampled_frame = input_frame
 
-        # Resample, and re-chunk. A None frame will flush the resampler,
-        # and then flush the fifo.
-        cdef AudioFrame resampled_frame = self.resampler.resample(input_frame)
         if resampled_frame:
-            self.fifo.write(resampled_frame)
-            # print 'resampled_frame.ptr.pts', resampled_frame.ptr.pts
 
-        # Pull partial frames if we were requested to flush (via a None frame).
-        cdef AudioFrame fifo_frame = self.fifo.read(self._codec_context.frame_size, partial=input_frame is None)
-
-        cdef Packet packet = Packet()
-        cdef int got_packet = 0
-
-        if fifo_frame:
-
-            # If the fifo_frame has a valid pts, scale it to the codec's time_base.
+            # If the resampled_frame has a valid pts, scale it to the codec's time_base.
             # Remember that the AudioFifo time_base is always 1/sample_rate!
-            if fifo_frame.ptr.pts != lib.AV_NOPTS_VALUE:
-                fifo_frame.ptr.pts = lib.av_rescale_q(
-                    fifo_frame.ptr.pts, 
-                    fifo_frame._time_base,
-                    self._codec_context.time_base
-                )
+            if resampled_frame.ptr.pts != lib.AV_NOPTS_VALUE:
+                resampled_frame.ptr.pts = lib.av_rescale_q(
+                    resampled_frame.ptr.pts,
+                    resampled_frame._time_base,
+                    self._codec_context.time_base)
             else:
-                fifo_frame.ptr.pts = lib.av_rescale(
+                resampled_frame.ptr.pts = lib.av_rescale(
                     self._codec_context.frame_number,
                     self._codec_context.sample_rate,
-                    self._codec_context.frame_size,
+                    self._codec_context.frame_size)
+
+        cdef Packet packet
+        for packet in self.coder.encode(resampled_frame):
+            # Rescale some times which are in the codec's time_base to the
+            # stream's time_base.
+            if packet.struct.pts != lib.AV_NOPTS_VALUE:
+                packet.struct.pts = lib.av_rescale_q(
+                    packet.struct.pts,
+                    self._codec_context.time_base,
+                    self._stream.time_base
                 )
-                
-        err_check(lib.avcodec_encode_audio2(
-            self._codec_context,
-            &packet.struct,
-            fifo_frame.ptr if fifo_frame is not None else NULL,
-            &got_packet,
-        ))
-        if not got_packet:
-            return
-        
-        # Rescale some times which are in the codec's time_base to the
-        # stream's time_base.
-        if packet.struct.pts != lib.AV_NOPTS_VALUE:
-            packet.struct.pts = lib.av_rescale_q(
-                packet.struct.pts,
-                self._codec_context.time_base,
-                self._stream.time_base
-            )
-        if packet.struct.dts != lib.AV_NOPTS_VALUE:
-            packet.struct.dts = lib.av_rescale_q(
-                packet.struct.dts,
-                self._codec_context.time_base,
-                self._stream.time_base
-            )
-        if packet.struct.duration > 0:
-            packet.struct.duration = lib.av_rescale_q(
-                packet.struct.duration,
-                self._codec_context.time_base,
-                self._stream.time_base
-            )
-           
-        # `coded_frame` is "the picture in the bitstream"; does this make
-        # sense for audio?  
-        if self._codec_context.coded_frame:
-            if self._codec_context.coded_frame.key_frame:
-                packet.struct.flags |= lib.AV_PKT_FLAG_KEY
+            if packet.struct.dts != lib.AV_NOPTS_VALUE:
+                packet.struct.dts = lib.av_rescale_q(
+                    packet.struct.dts,
+                    self._codec_context.time_base,
+                    self._stream.time_base
+                )
+            if packet.struct.duration > 0:
+                packet.struct.duration = lib.av_rescale_q(
+                    packet.struct.duration,
+                    self._codec_context.time_base,
+                    self._stream.time_base
+                )
 
-        packet.struct.stream_index = self._stream.index
-        packet.stream = self
-        
-        return packet
-        
+            # `coded_frame` is "the picture in the bitstream"; does this make
+            # sense for audio?
+            if self._codec_context.coded_frame:
+                if self._codec_context.coded_frame.key_frame:
+                    packet.struct.flags |= lib.AV_PKT_FLAG_KEY
 
+            packet.struct.stream_index = self._stream.index
+            packet.stream = self
+
+            yield packet
