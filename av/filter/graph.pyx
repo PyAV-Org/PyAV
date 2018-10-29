ed96cc9c87ac0a2420fbd2ea5f1c061f9a08ffad
egao1980 <egao1980@gmail.com>
2018-10-29 15:04:25 +0000

    Reorganize imports

--- av/filter/graph.pyx
+++ av/filter/graph.pyx
@@ -1,15 +1,13 @@
 from fractions import Fraction
 
-from libc.string cimport memcpy
-
-from av.audio.layout cimport AudioLayout
-from av.audio.frame cimport AudioFrame, alloc_audio_frame
 from av.audio.format cimport AudioFormat
+from av.audio.frame cimport AudioFrame
+from av.audio.layout cimport AudioLayout
 from av.filter.context cimport FilterContext, wrap_filter_context
 from av.filter.filter cimport Filter, wrap_filter
 from av.utils cimport err_check
-from av.video.frame cimport VideoFrame, alloc_video_frame
 from av.video.format cimport VideoFormat
+from av.video.frame cimport VideoFrame
 
 cdef class Graph(object):
 
