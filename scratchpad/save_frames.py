5b7441e782c4f0335c510f9aa831e29d253a8f4d
Mike Boers <github@mikeboers.com>
2018-11-07 17:49:31 -0500

    Remove old scratchpad.

--- scratchpad/save_frames.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-import av
-
-container = av.open(sys.argv[1])
-for i, frame in enumerate(container.decode(video=0)):
-    frame.to_image().save('sandbox/%04d.jpg' % i)
-    if i > 5:
-        break
