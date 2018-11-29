083c2645a617bc452e50bd97c2f6847db8e68aff
Mike Boers <github@mikeboers.com>
2018-11-29 11:06:49 -0500

    Fix parse flushing logic.

--- av/codec/context.pyx
+++ av/codec/context.pyx
@@ -177,7 +177,7 @@ cdef class CodecContext(object):
 
         :param ByteSource raw_input: A chunk of a byte-stream to process.
             Anything that can be turned into a :class:`.ByteSource` is fine.
-            ``None`` is used to flush the parser's buffers.
+            ``None`` or empty inputs will flush the parser's buffers.
 
         :return: ``list`` of :class:`.Packet` newly availible.
 
@@ -200,7 +200,7 @@ cdef class CodecContext(object):
 
         packets = []
 
-        while in_size:
+        while True:
 
             with nogil:
                 consumed = lib.av_parser_parse2(
@@ -232,8 +232,16 @@ cdef class CodecContext(object):
 
                 packets.append(packet)
 
+            if not in_size:
+                # This was a flush. Only one packet should ever be returned.
+                break
+
             in_data += consumed
             in_size -= consumed
+
+            if not in_size:
+                # Aaaand now we're done.
+                break
 
         return packets
 
