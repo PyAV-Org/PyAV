bd9efa8c6fad4fa368e7290b8281b3340371b8e6
Jeremy Lainé <jeremy.laine@m4x.org>
2021-01-22 16:04:24 +0100

    [package] mention Python 3.9 compatibility

--- setup.py
+++ setup.py
@@ -530,6 +530,7 @@ setup(
        'Programming Language :: Python :: 3.6',
        'Programming Language :: Python :: 3.7',
        'Programming Language :: Python :: 3.8',
+       'Programming Language :: Python :: 3.9',
        'Topic :: Software Development :: Libraries :: Python Modules',
        'Topic :: Multimedia :: Sound/Audio',
        'Topic :: Multimedia :: Sound/Audio :: Conversion',
