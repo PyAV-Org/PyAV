60cbc412b7c9fea7bac624ec06b168b58ff785a1
Jeremy Lainé <jeremy.laine@m4x.org>
2021-02-26 16:15:56 +0100

    Use av_guess_format for output container format (fixes: #691)
    Some formats go by multiple names, so checking for equality of the
    format name fails. Use the higher-level `av_guess_format` which does
    the right thing.

--- tests/test_containerformat.py
+++ tests/test_containerformat.py
@@ -6,15 +6,12 @@ from .common import TestCase
 class TestContainerFormats(TestCase):
 
     def test_matroska(self):
-
         fmt = ContainerFormat('matroska')
-
         self.assertTrue(fmt.is_input)
         self.assertTrue(fmt.is_output)
         self.assertEqual(fmt.name, 'matroska')
         self.assertEqual(fmt.long_name, 'Matroska')
         self.assertIn('mkv', fmt.extensions)
-
         self.assertFalse(fmt.no_file)
 
     def test_mov(self):
@@ -23,6 +20,26 @@ class TestContainerFormats(TestCase):
         self.assertTrue(fmt.is_output)
         self.assertEqual(fmt.name, 'mov')
         self.assertEqual(fmt.long_name, 'QuickTime / MOV')
+        self.assertIn('mov', fmt.extensions)
+        self.assertFalse(fmt.no_file)
+
+    def test_stream_segment(self):
+        # This format goes by two names, check both.
+        fmt = ContainerFormat('stream_segment')
+        self.assertFalse(fmt.is_input)
+        self.assertTrue(fmt.is_output)
+        self.assertEqual(fmt.name, 'stream_segment')
+        self.assertEqual(fmt.long_name, 'streaming segment muxer')
+        self.assertEqual(fmt.extensions, set())
+        self.assertTrue(fmt.no_file)
+
+        fmt = ContainerFormat('ssegment')
+        self.assertFalse(fmt.is_input)
+        self.assertTrue(fmt.is_output)
+        self.assertEqual(fmt.name, 'ssegment')
+        self.assertEqual(fmt.long_name, 'streaming segment muxer')
+        self.assertEqual(fmt.extensions, set())
+        self.assertTrue(fmt.no_file)
 
     def test_formats_available(self):
         self.assertTrue(formats_available)
