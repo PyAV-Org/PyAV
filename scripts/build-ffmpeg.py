662c0fd8d12300be76fa9d3abb1d01591114ea1a
Jeremy Lainé <jeremy.laine@m4x.org>
2022-02-21 15:57:22 +0100

    [fribidi] update to 1.0.10

--- scripts/build-ffmpeg.py
+++ scripts/build-ffmpeg.py
@@ -99,7 +99,7 @@ if not os.path.exists(output_tarball):
         ),
         Package(
             name="fribidi",
-            source_url="https://github.com/fribidi/fribidi/releases/download/v1.0.9/fribidi-1.0.9.tar.xz",
+            source_url="https://github.com/fribidi/fribidi/releases/download/v1.0.11/fribidi-1.0.11.tar.xz",
         ),
         Package(
             name="nettle",
