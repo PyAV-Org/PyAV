PyAV-Org/PyAV

commit 6443f55e7d07948a93cc03f417e3cb66227fd6bf

Replace deprecated av_init_packet with av_packet_alloc

av/container/output.pyx
@@ -210,18 +210,17 @@ cdef class OutputContainer(Container):
         self.start_encoding()
 
         # Assert the packet is in stream time.
-        if packet.struct.stream_index < 0 or <unsigned int>packet.struct.stream_index >= self.ptr.nb_streams:
+        if packet.ptr.stream_index < 0 or <unsigned int>packet.ptr.stream_index >= self.ptr.nb_streams:
             raise ValueError('Bad Packet stream_index.')
-        cdef lib.AVStream *stream = self.ptr.streams[packet.struct.stream_index]
+        cdef lib.AVStream *stream = self.ptr.streams[packet.ptr.stream_index]
         packet._rebase_time(stream.time_base)
 
         # Make another reference to the packet, as av_interleaved_write_frame
         # takes ownership of it.
-        cdef lib.AVPacket packet_ref
-        lib.av_init_packet(&packet_ref)
-        self.err_check(lib.av_packet_ref(&packet_ref, &packet.struct))
+        cdef lib.AVPacket *packet_ptr = lib.av_packet_alloc()
+        self.err_check(lib.av_packet_ref(packet_ptr, packet.ptr))
 
         cdef int ret
         with nogil:
-            ret = lib.av_interleaved_write_frame(self.ptr, &packet_ref)
+            ret = lib.av_interleaved_write_frame(self.ptr, packet_ptr)
         self.err_check(ret)

av/codec/context.pyx
@@ -376,7 +376,7 @@ cdef class CodecContext(object):
                 # ... but this results in corruption.
 
                 packet = Packet(out_size)
-                memcpy(packet.struct.data, out_data, out_size)
+                memcpy(packet.ptr.data, out_data, out_size)
 
                 packets.append(packet)
 
@@ -417,7 +417,7 @@ cdef class CodecContext(object):
 
         cdef int res
         with nogil:
-            res = lib.avcodec_send_packet(self.ptr, &packet.struct if packet is not None else NULL)
+            res = lib.avcodec_send_packet(self.ptr, packet.ptr if packet is not None else NULL)
         err_check(res)
 
         out = []
@@ -459,7 +459,7 @@ cdef class CodecContext(object):
 
         cdef int res
         with nogil:
-            res = lib.avcodec_receive_packet(self.ptr, &packet.struct)
+            res = lib.avcodec_receive_packet(self.ptr, packet.ptr)
         if res == -EAGAIN or res == lib.AVERROR_EOF:
             return
         err_check(res)

av/container/input.pyx
@@ -138,18 +138,18 @@ cdef class InputContainer(Container):
                 try:
                     self.start_timeout()
                     with nogil:
-                        ret = lib.av_read_frame(self.ptr, &packet.struct)
+                        ret = lib.av_read_frame(self.ptr, packet.ptr)
                     self.err_check(ret)
                 except EOFError:
                     break
 
-                if include_stream[packet.struct.stream_index]:
+                if include_stream[packet.ptr.stream_index]:
                     # If AVFMTCTX_NOHEADER is set in ctx_flags, then new streams
                     # may also appear in av_read_frame().
                     # http://ffmpeg.org/doxygen/trunk/structAVFormatContext.html
                     # TODO: find better way to handle this
-                    if packet.struct.stream_index < len(self.streams):
-                        packet._stream = self.streams[packet.struct.stream_index]
+                    if packet.ptr.stream_index < len(self.streams):
+                        packet._stream = self.streams[packet.ptr.stream_index]
                         # Keep track of this so that remuxing is easier.
                         packet._time_base = packet._stream._stream.time_base
                         yield packet

av/packet.pxd
@@ -7,7 +7,7 @@ from av.stream cimport Stream
 
 cdef class Packet(Buffer):
 
-    cdef lib.AVPacket struct
+    cdef lib.AVPacket* ptr
 
     cdef Stream _stream
 

av/packet.pyx
@@ -18,7 +18,7 @@ cdef class Packet(Buffer):
 
     def __cinit__(self, input=None):
         with nogil:
-            lib.av_init_packet(&self.struct)
+            self.ptr = lib.av_packet_alloc()
 
     def __init__(self, input=None):
 
@@ -35,7 +35,7 @@ cdef class Packet(Buffer):
             size = source.length
 
         if size:
-            err_check(lib.av_new_packet(&self.struct, size))
+            err_check(lib.av_new_packet(self.ptr, size))
 
         if source is not None:
             self.update(source)
@@ -45,7 +45,7 @@ cdef class Packet(Buffer):
 
     def __dealloc__(self):
         with nogil:
-            lib.av_packet_unref(&self.struct)
+            lib.av_packet_free(&self.ptr)
 
     def __repr__(self):
         return '<av.%s of #%d, dts=%s, pts=%s; %s bytes at 0x%x>' % (
@@ -53,15 +53,15 @@ cdef class Packet(Buffer):
             self._stream.index if self._stream else 0,
             self.dts,
             self.pts,
-            self.struct.size,
+            self.ptr.size,
             id(self),
         )
 
     # Buffer protocol.
     cdef size_t _buffer_size(self):
-        return self.struct.size
+        return self.ptr.size
     cdef void* _buffer_ptr(self):
-        return self.struct.data
+        return self.ptr.data
 
     cdef _rebase_time(self, lib.AVRational dst):
 
@@ -75,7 +75,7 @@ cdef class Packet(Buffer):
         if self._time_base.num == dst.num and self._time_base.den == dst.den:
             return
 
-        lib.av_packet_rescale_ts(&self.struct, self._time_base, dst)
+        lib.av_packet_rescale_ts(self.ptr, self._time_base, dst)
 
         self._time_base = dst
 
@@ -101,7 +101,7 @@ cdef class Packet(Buffer):
 
     property stream_index:
         def __get__(self):
-            return self.struct.stream_index
+            return self.ptr.stream_index
 
     property stream:
         """
@@ -112,7 +112,7 @@ cdef class Packet(Buffer):
 
         def __set__(self, Stream stream):
             self._stream = stream
-            self.struct.stream_index = stream._stream.index
+            self.ptr.stream_index = stream._stream.index
 
     property time_base:
         """
@@ -135,14 +135,14 @@ cdef class Packet(Buffer):
         :type: int
         """
         def __get__(self):
-            if self.struct.pts != lib.AV_NOPTS_VALUE:
-                return self.struct.pts
+            if self.ptr.pts != lib.AV_NOPTS_VALUE:
+                return self.ptr.pts
 
         def __set__(self, v):
             if v is None:
-                self.struct.pts = lib.AV_NOPTS_VALUE
+                self.ptr.pts = lib.AV_NOPTS_VALUE
             else:
-                self.struct.pts = v
+                self.ptr.pts = v
 
     property dts:
         """
@@ -151,14 +151,14 @@ cdef class Packet(Buffer):
         :type: int
         """
         def __get__(self):
-            if self.struct.dts != lib.AV_NOPTS_VALUE:
-                return self.struct.dts
+            if self.ptr.dts != lib.AV_NOPTS_VALUE:
+                return self.ptr.dts
 
         def __set__(self, v):
             if v is None:
-                self.struct.dts = lib.AV_NOPTS_VALUE
+                self.ptr.dts = lib.AV_NOPTS_VALUE
             else:
-                self.struct.dts = v
+                self.ptr.dts = v
 
     property pos:
         """
@@ -169,8 +169,8 @@ cdef class Packet(Buffer):
         :type: int
         """
         def __get__(self):
-            if self.struct.pos != -1:
-                return self.struct.pos
+            if self.ptr.pos != -1:
+                return self.ptr.pos
 
     property size:
         """
@@ -179,7 +179,7 @@ cdef class Packet(Buffer):
         :type: int
         """
         def __get__(self):
-            return self.struct.size
+            return self.ptr.size
 
     property duration:
         """
@@ -190,11 +190,11 @@ cdef class Packet(Buffer):
         :type: int
         """
         def __get__(self):
-            if self.struct.duration != lib.AV_NOPTS_VALUE:
-                return self.struct.duration
+            if self.ptr.duration != lib.AV_NOPTS_VALUE:
+                return self.ptr.duration
 
     property is_keyframe:
-        def __get__(self): return bool(self.struct.flags & lib.AV_PKT_FLAG_KEY)
+        def __get__(self): return bool(self.ptr.flags & lib.AV_PKT_FLAG_KEY)
 
     property is_corrupt:
-        def __get__(self): return bool(self.struct.flags & lib.AV_PKT_FLAG_CORRUPT)
+        def __get__(self): return bool(self.ptr.flags & lib.AV_PKT_FLAG_CORRUPT)

av/stream.pyx
@@ -156,7 +156,7 @@ cdef class Stream(object):
         cdef Packet packet
         for packet in packets:
             packet._stream = self
-            packet.struct.stream_index = self._stream.index
+            packet.ptr.stream_index = self._stream.index
         return packets
 
     def decode(self, packet=None):

av/subtitles/codeccontext.pyx
@@ -16,7 +16,7 @@ cdef class SubtitleCodecContext(CodecContext):
             self.ptr,
             &proxy.struct,
             &got_frame,
-            &packet.struct if packet else NULL))
+            packet.ptr if packet else NULL))
         if got_frame:
             return [SubtitleSet(proxy)]
         else:

include/libavcodec/avcodec.pxd
@@ -338,8 +338,6 @@ cdef extern from "libavcodec/avcodec.h" nogil:
 
         int64_t pos
 
-        void (*destruct)(AVPacket*)
-
 
     cdef int avcodec_fill_audio_frame(
         AVFrame *frame,
@@ -352,10 +350,10 @@ cdef extern from "libavcodec/avcodec.h" nogil:
 
     cdef void avcodec_free_frame(AVFrame **frame)
 
-    cdef void av_init_packet(AVPacket*)
+    cdef AVPacket* av_packet_alloc()
+    cdef void av_packet_free(AVPacket **)
     cdef int av_new_packet(AVPacket*, int)
     cdef int av_packet_ref(AVPacket *dst, const AVPacket *src)
-    cdef void av_packet_unref(AVPacket *pkt)
     cdef void av_packet_rescale_ts(AVPacket *pkt, AVRational src_tb, AVRational dst_tb)
 
     cdef enum AVSubtitleType:

commit e5785e313e9dcb9a63b96e83cc672ae0ce3315f1

Allocate packet in OutputContainer to use for muxing
Fixes a memory leak introduced in 6443f55 when moving from
av_init_packet to av_packet_alloc.
Updated comment to make clear that av_interleaved_write_frame takes
ownership of the reference inside the packet and not the packet itself.

av/container/output.pxd
@@ -8,5 +8,6 @@ cdef class OutputContainer(Container):
 
     cdef bint _started
     cdef bint _done
+    cdef lib.AVPacket *packet_ptr
 
     cpdef start_encoding(self)

av/container/output.pyx
@@ -34,9 +34,13 @@ cdef class OutputContainer(Container):
     def __cinit__(self, *args, **kwargs):
         self.streams = StreamContainer()
         self.metadata = {}
+        with nogil:
+            self.packet_ptr = lib.av_packet_alloc()
 
     def __dealloc__(self):
         close_output(self)
+        with nogil:
+            lib.av_packet_free(&self.packet_ptr)
 
     def add_stream(self, codec_name=None, object rate=None, Stream template=None, options=None, **kwargs):
         """add_stream(codec_name, rate=None)
@@ -219,11 +223,10 @@ cdef class OutputContainer(Container):
         packet._rebase_time(stream.time_base)
 
         # Make another reference to the packet, as av_interleaved_write_frame
-        # takes ownership of it.
-        cdef lib.AVPacket *packet_ptr = lib.av_packet_alloc()
-        self.err_check(lib.av_packet_ref(packet_ptr, packet.ptr))
+        # takes ownership of the reference.
+        self.err_check(lib.av_packet_ref(self.packet_ptr, packet.ptr))
 
         cdef int ret
         with nogil:
-            ret = lib.av_interleaved_write_frame(self.ptr, packet_ptr)
+            ret = lib.av_interleaved_write_frame(self.ptr, self.packet_ptr)
         self.err_check(ret)