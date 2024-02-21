6bc962116c397497bab2f9bfcc9a21b72a8d527a
JoeUgly <41972063+JoeUgly@users.noreply.github.com>
2024-02-21 18:43:42 -0500

    Convert deprecated cython extension class properties to new syntax part 1

--- av/option.pyx
+++ av/option.pyx
@@ -64,82 +64,82 @@ cdef class BaseOption:
         if sentinel is not _cinit_sentinel:
             raise RuntimeError(f"Cannot construct av.{self.__class__.__name__}")
 
-    property name:
-        def __get__(self):
-            return self.ptr.name
+    @property
+    def name(self):
+        return self.ptr.name
 
-    property help:
-        def __get__(self):
-            return self.ptr.help if self.ptr.help != NULL else ""
+    @property
+    def help(self):
+        return self.ptr.help if self.ptr.help != NULL else ""
 
-    property flags:
-        def __get__(self):
-            return self.ptr.flags
+    @property
+    def flags(self):
+        return self.ptr.flags
 
     # Option flags
-    property is_encoding_param:
-        def __get__(self):
-            return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_ENCODING_PARAM)
-    property is_decoding_param:
-        def __get__(self):
-            return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_DECODING_PARAM)
-    property is_audio_param:
-        def __get__(self):
-            return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_AUDIO_PARAM)
-    property is_video_param:
-        def __get__(self):
-            return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_VIDEO_PARAM)
-    property is_subtitle_param:
-        def __get__(self):
-            return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_SUBTITLE_PARAM)
-    property is_export:
-        def __get__(self):
-            return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_EXPORT)
-    property is_readonly:
-        def __get__(self):
-            return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_READONLY)
-    property is_filtering_param:
-        def __get__(self):
-            return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_FILTERING_PARAM)
+    @property
+    def is_encoding_param(self):
+        return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_ENCODING_PARAM)
+    @property
+    def is_decoding_param(self):
+        return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_DECODING_PARAM)
+    @property
+    def is_audio_param(self):
+        return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_AUDIO_PARAM)
+    @property
+    def is_video_param(self):
+        return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_VIDEO_PARAM)
+    @property
+    def is_subtitle_param(self):
+        return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_SUBTITLE_PARAM)
+    @property
+    def is_export(self):
+        return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_EXPORT)
+    @property
+    def is_readonly(self):
+        return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_READONLY)
+    @property
+    def is_filtering_param(self):
+        return flag_in_bitfield(self.ptr.flags, lib.AV_OPT_FLAG_FILTERING_PARAM)
 
 
 cdef class Option(BaseOption):
-    property type:
-        def __get__(self):
-            return OptionType._get(self.ptr.type, create=True)
+    @property
+    def type(self):
+        return OptionType._get(self.ptr.type, create=True)
 
-    property offset:
+    @property
+    def offset(self):
         """
         This can be used to find aliases of an option.
         Options in a particular descriptor with the same offset are aliases.
         """
-        def __get__(self):
-            return self.ptr.offset
+        return self.ptr.offset
 
-    property default:
-        def __get__(self):
-            if self.ptr.type in _INT_TYPES:
-                return self.ptr.default_val.i64
-            if self.ptr.type in (lib.AV_OPT_TYPE_DOUBLE, lib.AV_OPT_TYPE_FLOAT,
-                                 lib.AV_OPT_TYPE_RATIONAL):
-                return self.ptr.default_val.dbl
-            if self.ptr.type in (lib.AV_OPT_TYPE_STRING, lib.AV_OPT_TYPE_BINARY,
-                                 lib.AV_OPT_TYPE_IMAGE_SIZE, lib.AV_OPT_TYPE_VIDEO_RATE,
-                                 lib.AV_OPT_TYPE_COLOR):
-                return self.ptr.default_val.str if self.ptr.default_val.str != NULL else ""
+    @property
+    def default(self):
+        if self.ptr.type in _INT_TYPES:
+            return self.ptr.default_val.i64
+        if self.ptr.type in (lib.AV_OPT_TYPE_DOUBLE, lib.AV_OPT_TYPE_FLOAT,
+                             lib.AV_OPT_TYPE_RATIONAL):
+            return self.ptr.default_val.dbl
+        if self.ptr.type in (lib.AV_OPT_TYPE_STRING, lib.AV_OPT_TYPE_BINARY,
+                             lib.AV_OPT_TYPE_IMAGE_SIZE, lib.AV_OPT_TYPE_VIDEO_RATE,
+                             lib.AV_OPT_TYPE_COLOR):
+            return self.ptr.default_val.str if self.ptr.default_val.str != NULL else ""
 
     def _norm_range(self, value):
         if self.ptr.type in _INT_TYPES:
             return int(value)
         return value
 
-    property min:
-        def __get__(self):
-            return self._norm_range(self.ptr.min)
+    @property
+    def min(self):
+        return self._norm_range(self.ptr.min)
 
-    property max:
-        def __get__(self):
-            return self._norm_range(self.ptr.max)
+    @property
+    def max(self):
+        return self._norm_range(self.ptr.max)
 
     def __repr__(self):
         return (
@@ -164,9 +164,9 @@ cdef class OptionChoice(BaseOption):
     choices of non-const option with same unit.
     """
 
-    property value:
-        def __get__(self):
-            return self.ptr.default_val.i64
+    @property
+    def value(self):
+        return self.ptr.default_val.i64
 
     def __repr__(self):
         return f"<av.{self.__class__.__name__} {self.name} at 0x{id(self):x}>"
