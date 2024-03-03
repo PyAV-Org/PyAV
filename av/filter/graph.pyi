cf90e0e3fca153c7e635897a4cb5017297209104
laggykiller <chaudominic2@gmail.com>
2024-03-04 06:58:58 +0800

    Improve stubs in av.filter

--- av/filter/graph.pyi
+++ av/filter/graph.pyi
@@ -1,23 +1,32 @@
 from fractions import Fraction
 
+from av.audio.format import AudioFormat
 from av.audio.frame import AudioFrame
+from av.audio.layout import AudioLayout
 from av.audio.stream import AudioStream
+from av.video.format import VideoFormat
 from av.video.frame import VideoFrame
 from av.video.stream import VideoStream
 
 from .context import FilterContext
 from .filter import Filter
 
+from typing import Any
+
 class Graph:
+    configured: bool
+
     def __init__(self) -> None: ...
     def configure(self, auto_buffer: bool = True, force: bool = False) -> None: ...
-    def add(self, filter: str | Filter, args=None, **kwargs: str) -> FilterContext: ...
+    def add(
+        self, filter: str | Filter, args: Any = None, **kwargs: str
+    ) -> FilterContext: ...
     def add_buffer(
         self,
         template: VideoStream | None = None,
         width: int | None = None,
         height: int | None = None,
-        format=None,
+        format: VideoFormat | None = None,
         name: str | None = None,
         time_base: Fraction | None = None,
     ) -> FilterContext: ...
@@ -25,8 +34,8 @@ class Graph:
         self,
         template: AudioStream | None = None,
         sample_rate: int | None = None,
-        format=None,
-        layout=None,
+        format: AudioFormat | None = None,
+        layout: AudioLayout | None = None,
         channels: int | None = None,
         name: str | None = None,
         time_base: Fraction | None = None,
