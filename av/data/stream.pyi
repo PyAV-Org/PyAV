1c49223f1ad9460be4fd1f24fe5492ad7c395376
laggykiller <chaudominic2@gmail.com>
2024-03-04 06:59:32 +0800

    Improve stub in av.data

--- av/data/stream.pyi
+++ av/data/stream.pyi
@@ -3,5 +3,7 @@ from av.packet import Packet
 from av.stream import Stream
 
 class DataStream(Stream):
+    type = "data"
+
     def encode(self, frame: Frame | None = None) -> list[Packet]: ...
     def decode(self, packet: Packet | None = None, count: int = 0) -> list[Frame]: ...
