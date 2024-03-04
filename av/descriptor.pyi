7bfb8e7399a40dc17c6c9ef8f7f7fe66d4e9dcb1
WyattBlue <wyattblue@auto-editor.com>
2024-03-04 10:16:15 -0500

    fix

--- av/descriptor.pyi
+++ av/descriptor.pyi
@@ -5,5 +5,3 @@ from .option import Option
 class Descriptor:
     name: str
     options: tuple[Option, ...]
-
-    def __init__(self) -> NoReturn: ...
--- av/plane.pyi
+++ av/plane.pyi
@@ -4,6 +4,5 @@ from .frame import Frame
 class Plane(Buffer):
     frame: Frame
     index: int
-    buffer_size: int
 
     def __init__(self, frame: Frame, index: int) -> None: ...
