88e9f0f82cb8757312570f60005a72bb83133a76
laggykiller <chaudominic2@gmail.com>
2024-03-04 08:36:20 +0800

    Fix typing

--- av/descriptor.pyi
+++ av/descriptor.pyi
@@ -1,7 +1,9 @@
+from typing import NoReturn
+
 from .option import Option
 
 class Descriptor:
     name: str
     options: tuple[Option, ...]
 
-    def __init__(self, sentinel: object) -> None: ...
+    def __init__(self) -> NoReturn: ...
