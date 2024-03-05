07aad66b85f046195459d506ae73ae0a4b6794ed
laggykiller <chaudominic2@gmail.com>
2024-03-05 12:10:44 +0800

    Also add codec_context typing in AudioStream

--- av/audio/stream.pyi
+++ av/audio/stream.pyi
@@ -1,3 +1,16 @@
+from typing import Literal
+
+from av.packet import Packet
 from av.stream import Stream
 
-class AudioStream(Stream): ...
+from .codeccontext import AudioCodecContext
+from .format import AudioFormat
+from .frame import AudioFrame
+
+class AudioStream(Stream):
+    format: AudioFormat
+    codec_context: AudioCodecContext
+    type: Literal["audio"]
+
+    def encode(self, frame: AudioFrame | None = None) -> list[Packet]: ...
+    def decode(self, packet: Packet | None = None) -> list[AudioFrame]: ...
