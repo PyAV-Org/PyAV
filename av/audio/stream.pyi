4e485b3c77ff28615ee6da6522203ffe3d99f23d
laggykiller <chaudominic2@gmail.com>
2024-03-05 11:46:51 +0800

    Fix literal typing

--- av/audio/stream.pyi
+++ av/audio/stream.pyi
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from av.packet import Packet
 from av.stream import Stream
 
@@ -6,7 +8,7 @@ from .frame import AudioFrame
 
 class AudioStream(Stream):
     format: AudioFormat
-    type = "audio"
+    type: Literal["audio"]
 
     def encode(self, frame: AudioFrame | None = None) -> list[Packet]: ...
     def decode(self, packet: Packet | None = None) -> list[AudioFrame]: ...
