d6a66c5b5a2fad30e13db8d8e18af556b578c370
WyattBlue <wyattblue@auto-editor.com>
2024-03-09 07:59:46 -0500

    Only VideoStream now has avg_rate, base, guessed

--- av/video/stream.pyx
+++ av/video/stream.pyx
@@ -1,5 +1,10 @@
+cimport libav as lib
+
 from av.packet cimport Packet
+from av.utils cimport avrational_to_fraction, to_avrational
+
 from .frame cimport VideoFrame
+
 
 cdef class VideoStream(Stream):
     def __repr__(self):
@@ -35,3 +40,41 @@ cdef class VideoStream(Stream):
         """
 
         return self.codec_context.decode(packet)
+
+    @property
+    def average_rate(self):
+        """
+        The average frame rate of this video stream.
+
+        This is calculated when the file is opened by looking at the first
+        few frames and averaging their rate.
+
+        :type: :class:`~fractions.Fraction` or ``None``
+        """
+        return avrational_to_fraction(&self.ptr.avg_frame_rate)
+
+    @property
+    def base_rate(self):
+        """
+        The base frame rate of this stream.
+
+        This is calculated as the lowest framerate at which the timestamps of
+        frames can be represented accurately. See :ffmpeg:`AVStream.r_frame_rate`
+        for more.
+
+        :type: :class:`~fractions.Fraction` or ``None``
+        """
+        return avrational_to_fraction(&self.ptr.r_frame_rate)
+
+    @property
+    def guessed_rate(self):
+        """The guessed frame rate of this stream.
+
+        This is a wrapper around :ffmpeg:`av_guess_frame_rate`, and uses multiple
+        heuristics to decide what is "the" frame rate.
+
+        :type: :class:`~fractions.Fraction` or ``None``
+        """
+        # The two NULL arguments aren't used in FFmpeg >= 4.0
+        cdef lib.AVRational val = lib.av_guess_frame_rate(NULL, self.ptr, NULL)
+        return avrational_to_fraction(&val)
