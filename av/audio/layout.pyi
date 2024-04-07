fba3f6c9657f28662ac5f936acc015ec5d331daf
WyattBlue <wyattblue@auto-editor.com>
2024-04-07 13:47:10 -0400

    Add type stubs for datasets.py

--- av/audio/layout.pyi
+++ av/audio/layout.pyi
@@ -5,6 +5,7 @@ class AudioLayout:
     layout: int
     nb_channels: int
     channels: tuple[AudioChannel, ...]
+    def __init__(self, layout: int | str | AudioLayout): ...
 
 class AudioChannel:
     name: str
