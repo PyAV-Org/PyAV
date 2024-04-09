9a161321cd128ea5b4a91a6df14b78693ba91c3e
Santtu Keskinen <santtu.keskinen@gmail.com>
2024-04-09 10:45:54 +0300

    Change ' -> " to appease linter

--- tests/test_bitstream.py
+++ tests/test_bitstream.py
@@ -8,27 +8,27 @@ from .common import TestCase, fate_suite
 class TestBitStreamFilters(TestCase):
 
     def test_filters_availible(self):
-        self.assertIn('h264_mp4toannexb', bitstream_filters_available)
+        self.assertIn("h264_mp4toannexb", bitstream_filters_available)
 
     def test_filter_chomp(self):
-        ctx = BitStreamFilterContext('chomp')
+        ctx = BitStreamFilterContext("chomp")
 
-        src_packets = [Packet(b'\x0012345\0\0\0'), None]
-        self.assertEqual(bytes(src_packets[0]), b'\x0012345\0\0\0')
+        src_packets = [Packet(b"\x0012345\0\0\0"), None]
+        self.assertEqual(bytes(src_packets[0]), b"\x0012345\0\0\0")
 
         result_packets = []
         for p in src_packets:
             result_packets.extend(ctx.filter(p))
 
         self.assertEqual(len(result_packets), 1)
-        self.assertEqual(bytes(result_packets[0]), b'\x0012345')
+        self.assertEqual(bytes(result_packets[0]), b"\x0012345")
 
     def test_filter_setts(self):
-        ctx = BitStreamFilterContext('setts=pts=N')
+        ctx = BitStreamFilterContext("setts=pts=N")
 
-        p1 = Packet(b'\0')
+        p1 = Packet(b"\0")
         p1.pts = 42
-        p2 = Packet(b'\0')
+        p2 = Packet(b"\0")
         p2.pts = 50
         src_packets = [p1, p2, None]
 
@@ -43,11 +43,11 @@ class TestBitStreamFilters(TestCase):
     def test_filter_h264_mp4toannexb(self):
         def is_annexb(packet):
             data = bytes(packet)
-            return data[:3] == b'\0\0\x01' or data[:4] == b'\0\0\0\x01'
+            return data[:3] == b"\0\0\x01" or data[:4] == b"\0\0\0\x01"
 
-        with av.open(fate_suite("h264/interlaced_crop.mp4"), 'r') as container:
+        with av.open(fate_suite("h264/interlaced_crop.mp4"), "r") as container:
             stream = container.streams.video[0]
-            ctx = BitStreamFilterContext('h264_mp4toannexb', stream)
+            ctx = BitStreamFilterContext("h264_mp4toannexb", stream)
 
             res_packets = []
             for p in container.demux(stream):
