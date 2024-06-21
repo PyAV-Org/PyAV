584b47bc9d65f29703e29a2679bdfb712bd2d47f
Jeremy Lainé <jeremy.laine@m4x.org>
2024-06-21 09:45:24 +0200

    Add type definitions for `AudioCodecContext` encoding / decoding
    The `encode`, `encode_lazy` and `decode` methods were missing type
    definitions.

--- av/audio/codeccontext.pyi
+++ av/audio/codeccontext.pyi
@@ -1,8 +1,10 @@
-from typing import Literal
+from typing import Iterator, Literal
 
 from av.codec.context import CodecContext
+from av.packet import Packet
 
 from .format import AudioFormat
+from .frame import AudioFrame
 from .layout import AudioLayout
 
 class AudioCodecContext(CodecContext):
@@ -14,3 +16,7 @@ class AudioCodecContext(CodecContext):
     layout: AudioLayout
     format: AudioFormat
     type: Literal["audio"]
+
+    def encode(self, frame: AudioFrame | None = None) -> list[Packet]: ...
+    def encode_lazy(self, frame: AudioFrame | None = None) -> Iterator[Packet]: ...
+    def decode(self, packet: Packet | None = None) -> list[AudioFrame]: ...
