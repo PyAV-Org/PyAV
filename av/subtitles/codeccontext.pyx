3cef0023450384f75ee7e9c2ac791312f8b283cd
WyattBlue <wyattblue@auto-editor.com>
2024-07-17 13:11:43 -0400

    Fix segfault when calling subtitle_stream.decode()

--- av/subtitles/codeccontext.pyx
+++ av/subtitles/codeccontext.pyx
@@ -7,13 +7,14 @@ from av.subtitles.subtitle cimport SubtitleProxy, SubtitleSet
 
 cdef class SubtitleCodecContext(CodecContext):
     cdef _send_packet_and_recv(self, Packet packet):
+        if packet is None:
+            raise RuntimeError("packet cannot be None")
+
         cdef SubtitleProxy proxy = SubtitleProxy()
         cdef int got_frame = 0
 
         err_check(
-            lib.avcodec_decode_subtitle2(
-                self.ptr, &proxy.struct, &got_frame, packet.ptr if packet else NULL
-            )
+            lib.avcodec_decode_subtitle2(self.ptr, &proxy.struct, &got_frame, packet.ptr)
         )
 
         if got_frame:
