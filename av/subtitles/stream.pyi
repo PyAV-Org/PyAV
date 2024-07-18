d70b66a33008a1832249d80649ba148588fa90f1
Moonsik Park <moonsik.park@estsoft.com>
2024-07-18 13:52:39 +0900

    flushing subtitle decoder requires a new uninitialized packet.
    lint changes
    
    rgq

--- av/subtitles/stream.pyi
+++ av/subtitles/stream.pyi
@@ -1,3 +1,6 @@
+from av.packet import Packet
 from av.stream import Stream
+from av.subtitles.subtitle import SubtitleSet
 
-class SubtitleStream(Stream): ...
+class SubtitleStream(Stream):
+    def decode(self, packet: Packet | None = None) -> list[SubtitleSet]: ...
