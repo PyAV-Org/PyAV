64612012be2e0d6005d4f2d398d3ba1384fc7d4c
WyattBlue <wyattblue@auto-editor.com>
2024-08-19 04:58:48 -0400

    Add back channels property to AudioLayout

--- av/audio/layout.pyi
+++ av/audio/layout.pyi
@@ -1,4 +1,12 @@
+from dataclasses import dataclass
+
 class AudioLayout:
     name: str
     nb_channels: int
+    channels: tuple[AudioChannel, ...]
     def __init__(self, layout: str | AudioLayout): ...
+
+@dataclass
+class AudioChannel:
+    name: str
+    description: str
