70d3e3ddbd14005f58cb1c84e751dfc369d69d17
WyattBlue <wyattblue@auto-editor.com>
2024-09-18 12:28:25 -0400

    Add smart overloading for `add_stream()`

--- av/video/format.pyi
+++ av/video/format.pyi
@@ -7,6 +7,8 @@ class VideoFormat:
     is_bit_stream: bool
     is_planar: bool
     is_rgb: bool
+    width: int
+    height: int
 
     def __init__(self, name: str, width: int = 0, height: int = 0) -> None: ...
     def chroma_width(self, luma_width: int = 0) -> int: ...
