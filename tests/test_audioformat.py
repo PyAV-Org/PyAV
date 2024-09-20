827bebd5d24449bfa95438bef3ff0d7a33a6f8fd
WyattBlue <wyattblue@auto-editor.com>
2024-09-19 23:47:17 -0400

    Add type stubs for test_audioformat, audioframe

--- tests/test_audioformat.py
+++ tests/test_audioformat.py
@@ -8,20 +8,20 @@ postfix = "le" if sys.byteorder == "little" else "be"
 
 
 class TestAudioFormats(TestCase):
-    def test_s16_inspection(self):
+    def test_s16_inspection(self) -> None:
         fmt = AudioFormat("s16")
-        self.assertEqual(fmt.name, "s16")
-        self.assertFalse(fmt.is_planar)
-        self.assertEqual(fmt.bits, 16)
-        self.assertEqual(fmt.bytes, 2)
-        self.assertEqual(fmt.container_name, "s16" + postfix)
-        self.assertEqual(fmt.planar.name, "s16p")
-        self.assertIs(fmt.packed, fmt)
+        assert fmt.name == "s16"
+        assert not fmt.is_planar
+        assert fmt.bits == 16
+        assert fmt.bytes == 2
+        assert fmt.container_name == "s16" + postfix
+        assert fmt.planar.name == "s16p"
+        assert fmt.packed is fmt
 
-    def test_s32p_inspection(self):
+    def test_s32p_inspection(self) -> None:
         fmt = AudioFormat("s32p")
-        self.assertEqual(fmt.name, "s32p")
-        self.assertTrue(fmt.is_planar)
-        self.assertEqual(fmt.bits, 32)
-        self.assertEqual(fmt.bytes, 4)
+        assert fmt.name == "s32p"
+        assert fmt.is_planar
+        assert fmt.bits == 32
+        assert fmt.bytes == 4
         self.assertRaises(ValueError, lambda: fmt.container_name)
