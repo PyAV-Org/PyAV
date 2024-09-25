45bad44a3900a9be3b82861ab5f5fd07d89fbf69
WyattBlue <wyattblue@auto-editor.com>
2024-09-25 05:26:16 -0400

    If Conda needs this, they can patch it themselves

--- av/__init__.py
+++ av/__init__.py
@@ -1,20 +1,3 @@
-import os
-import sys
-
-# Some Python versions distributed by Conda have a buggy `os.add_dll_directory`
-# which prevents binary wheels from finding the FFmpeg DLLs in the `av.libs`
-# directory. We work around this by adding `av.libs` to the PATH.
-if (
-    os.name == "nt"
-    and sys.version_info[:2] in ((3, 8), (3, 9))
-    and os.path.exists(os.path.join(sys.base_prefix, "conda-meta"))
-):
-    os.environ["PATH"] = (
-        os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "av.libs"))
-        + os.pathsep
-        + os.environ["PATH"]
-    )
-
 # MUST import the core before anything else in order to initialize the underlying
 # library that is being wrapped.
 from av._core import time_base, library_versions
@@ -77,6 +60,8 @@ def get_include() -> str:
     """
     Returns the path to the `include` folder to be used when building extensions to av.
     """
+    import os
+
     # Installed package
     include_path = os.path.join(os.path.dirname(__file__), "include")
     if os.path.exists(include_path):
