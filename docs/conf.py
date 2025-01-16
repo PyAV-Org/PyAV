24ad9e051d14840bad305002e5d26387732ad8a4
WyattBlue <wyattblue@auto-editor.com>
2025-01-16 12:44:59 -0500

    Change copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "PyAV"
-copyright = "2024, The PyAV Team"
+copyright = "2025, The PyAV Team"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
