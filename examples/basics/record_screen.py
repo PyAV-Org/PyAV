a4a9b1cd22362022062cd11b7fbb0140bcf2cac0
WyattBlue <wyattblue@auto-editor.com>
2025-03-09 13:12:29 -0400

    Update recording examples

--- examples/basics/record_screen.py
+++ examples/basics/record_screen.py
@@ -13,15 +13,25 @@ You may need to change the file "1". Use this command to list all devices:
 input_ = av.open("1", format="avfoundation")
 output = av.open("out.mkv", "w")
 
-output_stream = output.add_stream("h264", rate=30)
+# Prefer x264, but use Apple hardware if not available.
+try:
+    encoder = av.Codec("libx264", "w").name
+except av.FFmpegError:
+    encoder = "h264_videotoolbox"
+
+output_stream = output.add_stream(encoder, rate=30)
 output_stream.width = input_.streams.video[0].width
 output_stream.height = input_.streams.video[0].height
 output_stream.pix_fmt = "yuv420p"
 
 try:
-    for frame in input_.decode(video=0):
-        packet = output_stream.encode(frame)
-        output.mux(packet)
+    while True:
+        try:
+            for frame in input_.decode(video=0):
+                packet = output_stream.encode(frame)
+                output.mux(packet)
+        except av.BlockingIOError:
+            pass
 except KeyboardInterrupt:
     print("Recording stopped by user")
 
