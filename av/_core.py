d3fbbd2ab737dcea6aa9d686b899ea2177fcc2b2
WyattBlue <wyattblue@auto-editor.com>
2026-01-19 01:47:43 -0500

    Make av/core pure

--- av/_core.py
+++ av/_core.py
@@ -1,21 +1,22 @@
-cimport libav as lib
+import cython
+import cython.cimports.libav as lib
 
-# Initialise libraries.
 lib.avdevice_register_all()
 
 # Exports.
 time_base = lib.AV_TIME_BASE
 
 
-cdef decode_version(v):
+@cython.cfunc
+def decode_version(v):
     if v < 0:
         return (-1, -1, -1)
 
-    cdef int major = (v >> 16) & 0xff
-    cdef int minor = (v >> 8) & 0xff
-    cdef int micro = (v) & 0xff
-
+    major: cython.int = (v >> 16) & 0xFF
+    minor: cython.int = (v >> 8) & 0xFF
+    micro: cython.int = (v) & 0xFF
     return (major, minor, micro)
+
 
 # Return an informative version string.
 # This usually is the actual release version number or a git commit
@@ -27,37 +28,37 @@ library_meta = {
     "libavutil": dict(
         version=decode_version(lib.avutil_version()),
         configuration=lib.avutil_configuration(),
-        license=lib.avutil_license()
+        license=lib.avutil_license(),
     ),
     "libavcodec": dict(
         version=decode_version(lib.avcodec_version()),
         configuration=lib.avcodec_configuration(),
-        license=lib.avcodec_license()
+        license=lib.avcodec_license(),
     ),
     "libavformat": dict(
         version=decode_version(lib.avformat_version()),
         configuration=lib.avformat_configuration(),
-        license=lib.avformat_license()
+        license=lib.avformat_license(),
     ),
     "libavdevice": dict(
         version=decode_version(lib.avdevice_version()),
         configuration=lib.avdevice_configuration(),
-        license=lib.avdevice_license()
+        license=lib.avdevice_license(),
     ),
     "libavfilter": dict(
         version=decode_version(lib.avfilter_version()),
         configuration=lib.avfilter_configuration(),
-        license=lib.avfilter_license()
+        license=lib.avfilter_license(),
     ),
     "libswscale": dict(
         version=decode_version(lib.swscale_version()),
         configuration=lib.swscale_configuration(),
-        license=lib.swscale_license()
+        license=lib.swscale_license(),
     ),
     "libswresample": dict(
         version=decode_version(lib.swresample_version()),
         configuration=lib.swresample_configuration(),
-        license=lib.swresample_license()
+        license=lib.swresample_license(),
     ),
 }
 
