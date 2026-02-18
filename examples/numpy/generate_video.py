80da805da2ec36d53f3acefdabc29eafa15c9bdd
Benedikt Lorch <benedikt@getreallabs.com>
2026-02-18 09:23:34 +0100

    Switch height and width

--- examples/numpy/generate_video.py
+++ examples/numpy/generate_video.py
@@ -14,7 +14,7 @@ stream.height = 320
 stream.pix_fmt = "yuv420p"
 
 for frame_i in range(total_frames):
-    img = np.empty((480, 320, 3))
+    img = np.empty((320, 480, 3))
     img[:, :, 0] = 0.5 + 0.5 * np.sin(2 * np.pi * (0 / 3 + frame_i / total_frames))
     img[:, :, 1] = 0.5 + 0.5 * np.sin(2 * np.pi * (1 / 3 + frame_i / total_frames))
     img[:, :, 2] = 0.5 + 0.5 * np.sin(2 * np.pi * (2 / 3 + frame_i / total_frames))
